bbd7c0fdbd3bcc3b56a5e5a6c056cb0547c41076
Vikash Durairaj <vikash.durairaj@valuemomentum.com>
2026-08-10 14:20:24 +0530

    add dockerfile

--- SpaceBook.API/obj/Debug/net8.0/SpaceBook.API.AssemblyInfo.cs
+++ SpaceBook.API/obj/Debug/net8.0/SpaceBook.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpaceBook.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+024e46dc8fe7d1ee5600449ef3176492027675d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+166252e7b8b25ff393ec26470020688e93984a5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpaceBook.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpaceBook.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
