be77ddae0762a2a98d8535002ab4927afc6e8d5d
Vikash Durairaj <vikash.durairaj@valuemomentum.com>
2026-08-11 15:13:31 +0530

    program.cs

--- SpaceBook.API/Program.cs
+++ SpaceBook.API/Program.cs
@@ -64,7 +64,7 @@ builder.Services.AddCors(options =>
 
                 "https://spacebook211-98989-cs4tdldnv-dvikash211-5241s-projects.vercel.app",
                 "https://spacebook211-98989-acn15v4y5-dvikash211-5241s-projects.vercel.app",
-                "spacebook211-98989-g6itccmhu-dvikash211-5241s-projects.vercel.app"
+                "https://spacebook211-98989-g6itccmhu-dvikash211-5241s-projects.vercel.app"
             )
             .AllowAnyHeader()
             .AllowAnyMethod();
