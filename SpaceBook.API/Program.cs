8d46e83e9f4e7b07f7b8019c3ab81e44ad769e25
amirthag <amirthag3@gmail.com>
2026-08-12 15:13:59 +0530

    Adding Facilities

--- SpaceBook.API/Program.cs
+++ SpaceBook.API/Program.cs
@@ -61,7 +61,7 @@ builder.Services.AddCors(options =>
         policy
             .WithOrigins(
                 "http://localhost:5173",
-"https://spacebook211-2ivcylw0o-dvikash211-5241s-projects.vercel.app",
+"https://spacebook211-cdb1wov53-dvikash211-5241s-projects.vercel.app",
                 "https://spacebook211-98989-cs4tdldnv-dvikash211-5241s-projects.vercel.app",
                 "https://spacebook211-98989-acn15v4y5-dvikash211-5241s-projects.vercel.app",
                 "https://spacebook211-98989-g6itccmhu-dvikash211-5241s-projects.vercel.app"
