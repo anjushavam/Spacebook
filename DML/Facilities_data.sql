eceb7f12cd3df32e40ec35a86ec00513f4916fa6
Shofia Mathivanan <shofia.mathivanan@valuemomentum.com>
2026-08-13 15:01:55 +0530

    Updated rooms data

--- DML/Facilities_data.sql
+++ DML/Facilities_data.sql
@@ -7,6 +7,6 @@ INSERT INTO Facilities
 VALUES
 (1, 'Projector', '105508'),
 (2, 'Whiteboard', '105508'),
-(3, 'TV Display', '105508');
+(3, 'TV ', '105508');
 
 SELECT * FROM Facilities;
--- DML/Rooms_data.sql
+++ DML/Rooms_data.sql
@@ -9,20 +9,21 @@ INSERT INTO Rooms
     RecordIngestedBy
 )
 VALUES
-('CB-05-E01-001', 3, 'Training Room 1', 30, 'Module 1 - Elcot Park - CMB', 'Available', '105508'),
+('CB-05-E02-012', 3, 'Training Room ', 50, 'Module 2 - Elcot Park - CMB', 'Available', '105508'),
 
-('CB-05-E01-002', 1, 'Conference Room 1', 12, 'Module 1 - Elcot Park - CMB', 'Available', '105508'),
+('CB-05-E01-001', 1, 'Conference Room ', 20, 'Module 1 - Elcot Park - CMB', 'Available', '105508'),
 
-('CB-05-E02-001', 2, 'Discussion Room 1', 8, 'Module 2 - Elcot Park - CMB', 'Available', '105508'),
+('CB-05-E01-003', 2, 'Discussion Room 1', 8, 'Module 1 - Elcot Park - CMB', 'Available', '105508'),
 
-('CB-05-E02-002', 2, 'Discussion Room 2', 8, 'Module 2 - Elcot Park - CMB', 'Available', '105508'),
+('CB-05-E01-005', 2, 'Discussion Room 2', 8, 'Module 1 - Elcot Park - CMB', 'Available', '105508'),
 
-('CB-05-E02-003', 2, 'Discussion Room 3', 8, 'Module 2 - Elcot Park - CMB', 'Available', '105508'),
+('CB-05-E02-001', 2, 'Discussion Room 3', 8, 'Module 2 - Elcot Park - CMB', 'Available', '105508'),
 
-('CB-05-E02-004', 2, 'Discussion Room 4', 8, 'Module 2 - Elcot Park - CMB', 'Available', '105508'),
+('CB-05-E02-002', 2, 'Discussion Room 4', 8, 'Module 2 - Elcot Park - CMB', 'Available', '105508'),
 
-('CB-05-E02-005', 2, 'Discussion Room 5', 8, 'Module 2 - Elcot Park - CMB', 'Available', '105508'),
+('CB-05-E02-007', 2, 'Discussion Room 5', 8, 'Module 2 - Elcot Park - CMB', 'Available', '105508'),
 
-('CB-05-E02-006', 2, 'Discussion Room 6', 8, 'Module 2 - Elcot Park - CMB', 'Available', '105508');
+('CB-05-E02-010', 2, 'Discussion Room 6', 8, 'Module 2 - Elcot Park - CMB', 'Available', '105508');
 
 SELECT * FROM Rooms;
+
