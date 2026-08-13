03d543f04db53622351a7546f7cc27afc421a6dd
Vikash Durairaj <vikash.durairaj@valuemomentum.com>
2026-08-13 12:37:21 +0530

    m

--- SpaceBook.Domain/obj/Debug/net8.0/SpaceBook.Domain.AssemblyInfo.cs
+++ SpaceBook.Domain/obj/Debug/net8.0/SpaceBook.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpaceBook.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19446640e3384fbd9a07f8a37c4a8fcd263ee202")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3552e3e814db72d3323224d33345469e9d29b780")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpaceBook.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpaceBook.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
