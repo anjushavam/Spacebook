7c417b96187caaad046a05ec99f5fd166e690986
amirthag <amirthag3@gmail.com>
2026-08-15 19:03:30 +0530

    Office time change

--- SpaceBook.API/Program.cs
+++ SpaceBook.API/Program.cs
@@ -63,6 +63,7 @@ builder.Services.AddCors(options =>
                 "http://localhost:5173",
 "https://spacebookss.netlify.app",
 "https://spacebook211-fcms-1bpe04jye-dvikash211-5241s-projects.vercel.app",
+"https://spacebook-frontend-v64o.onrender.com",
                 "https://spacebook211-98989-cs4tdldnv-dvikash211-5241s-projects.vercel.app",
                 "https://spacebook211-98989-acn15v4y5-dvikash211-5241s-projects.vercel.app",
                 "https://spacebook211-98989-g6itccmhu-dvikash211-5241s-projects.vercel.app"
